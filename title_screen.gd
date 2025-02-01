c303bd9827401f84d40b7e25d483150a124fe0e4
Lewis Chippendale <lewis.chippendale@hotmail.co.uk>
2025-02-01 22:31:08 +0000

    Score file check fix

--- title_screen.gd
+++ title_screen.gd
@@ -4,8 +4,19 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	
-	# Displays High score on main menu
-	var content = FileAccess.open("res://HighScore.txt", FileAccess.READ)
+	## Displays High score on main menu
+	
+	var path = "res://HighScore.txt"
+	#DirAccess.make_dir_absolute("res://hello.txt")
+	
+	# Checks if score file exists if not it makes one in the exe directory
+	if FileAccess.open(path, FileAccess.READ) == null:
+		var file = FileAccess.open(path, FileAccess.WRITE)
+		file.store_string("0")
+		file.close()
+	
+	#var content = FileAccess.open("res://HighScore.txt", FileAccess.READ)
+	var content = FileAccess.open(path, FileAccess.READ)
 	var high_score = content.get_as_text()
 	
 	$High_Score.text = "High Score:  " + high_score
